43fe031e141155a0cff7e615eaa7357207a5f50d
fazzaamiarso <fazzaamiarso@gmail.com>
2022-05-30 17:20:03 +0700

    setup: Configure trpc with prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,29 @@
-import type { InferGetServerSidePropsType } from "next";
 import Image from "next/image";
-import db from "../db/prisma";
+import { trpc } from "../utils/trpc";
 
-export const getServerSideProps = async () => {
-  const users = await db.user.findMany({
-    select: { avatar: true, id: true, name: true },
-  });
+function Home() {
+  const { data, isLoading } = trpc.useQuery(["user"]);
 
-  const feedbacks = await db.feedback.findMany({
-    select: { id: true, comments: true, title: true, description: true },
-  });
-  return {
-    props: {
-      users,
-      feedbacks,
-    },
-  };
-};
-
-function Home({
-  users,
-  feedbacks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main className='text-2xl font-bold text-cyan bg-darkblue'>
       Hello Somebody
-      <ul className='flex gap-2'>
-        {users.map((user) => {
+      <ul className='flex flex-col gap-1'>
+        {isLoading && "Loading users..."}
+        {data?.users.map((user) => {
           return (
-            <li key={user.id}>
+            <li
+              key={user.id}
+              className='flex gap-4 items-center text-normal font-normal'
+            >
               <Image
                 className='rounded-full'
+                src={user.avatar ?? ""}
                 width={50}
                 height={50}
-                src={user.avatar ?? ""}
-                alt={user.name}
+                alt={user.username}
               />
+              <p>{user.username}</p>
             </li>
-          );
-        })}
-      </ul>
-      <ul className='max-w-screen w-10/12'>
-        {feedbacks.map((feedback) => {
-          return (
-            <pre className='w-screen' key={feedback.id}>
-              {JSON.stringify(feedback)}
-            </pre>
           );
         })}
       </ul>
