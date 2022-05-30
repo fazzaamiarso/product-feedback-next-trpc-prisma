5f8db304b33b9f5e9f29113c743faeebdb3ea27a
fazzaamiarso <fazzaamiarso@gmail.com>
2022-05-30 16:13:15 +0700

    db: Finished seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,16 +27,16 @@ async function seed() {
     getFeedbacks().map(async (fb) => {
       return await prisma.feedback.create({
         data: {
-          userId: fb.userId,
-          category: fb.category as Feedback["category"],
-          status: fb.status as Feedback["status"],
+          userId: "1",
+          category: fb.category.toUpperCase() as Feedback["category"],
+          status: fb.status.toUpperCase() as Feedback["status"],
           description: fb.description,
           title: fb.title,
           upvotes: {
             createMany: { data: generateUpvotes(fb.upvotes) },
           },
           comments: {
-            createMany: { data: fb.comments },
+            createMany: { data: fb.comments ?? [] },
           },
         },
       });
@@ -114,6 +114,175 @@ function getFeedbacks() {
         },
       ],
     },
+    {
+      userId: "1",
+      id: 3,
+      title: "Q&A within the challenge hubs",
+      category: "FEATURE",
+      upvotes: 65,
+      status: "SUGGESTION",
+      description: "Challenge-specific Q&A would make for easy reference.",
+      comments: [
+        {
+          id: 5,
+          content:
+            "Much easier to get answers from devs who can relate, since they've either finished the challenge themselves or are in the middle of it.",
+          userId: "6",
+        },
+      ],
+    },
+    {
+      userId: "1",
+      id: 4,
+      title: "Add avatar/video upload to feedback",
+      category: "enhancement",
+      upvotes: 51,
+      status: "suggestion",
+      description: "avatars and screencasts can enhance comments on solutions.",
+      comments: [
+        {
+          id: 6,
+          content:
+            "Right now, there is no ability to add avatars while giving feedback which isn't ideal because I have to use another app to show what I mean",
+          userId: "7",
+        },
+        {
+          id: 7,
+          content:
+            "Yes I'd like to see this as well. Sometimes I want to add a short video or gif to explain the site's behavior..",
+          userId: "8",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Ability to follow others",
+      category: "feature",
+      upvotes: 42,
+      status: "suggestion",
+      description: "Stay updated on comments and solutions other people post.",
+      comments: [
+        {
+          id: 8,
+          content:
+            "I also want to be notified when devs I follow submit projects on FEM. Is in-app notification also in the pipeline?",
+          userId: "9",
+        },
+        {
+          userId: "10",
+          id: 9,
+          content:
+            "I've been saving the profile URLs of a few people and I check what they’ve been doing from time to time. Being able to follow them solves that",
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: "Preview avatars not loading",
+      category: "bug",
+      upvotes: 3,
+      status: "suggestion",
+      description:
+        "Challenge preview avatars are missing when you apply a filter.",
+    },
+    {
+      id: 7,
+      title: "More comprehensive reports",
+      category: "feature",
+      upvotes: 123,
+      status: "planned",
+      description:
+        "It would be great to see a more detailed breakdown of solutions.",
+      comments: [
+        {
+          id: 10,
+          content:
+            "This would be awesome! It would be so helpful to see an overview of my code in a way that makes it easy to spot where things could be improved.",
+          userId: "11",
+        },
+        {
+          id: 11,
+          content:
+            "Yeah, this would be really good. I'd love to see deeper insights into my code!",
+          userId: "13",
+        },
+      ],
+    },
+    {
+      id: 8,
+      title: "Learning paths",
+      category: "feature",
+      upvotes: 28,
+      status: "planned",
+      description:
+        "Sequenced projects for different goals to help people improve.",
+      comments: [
+        {
+          id: 12,
+          content:
+            "Having a path through the challenges that I could follow would be brilliant! Sometimes I'm not sure which challenge would be the best next step to take. So this would help me navigate through them!",
+          userId: "8",
+        },
+      ],
+    },
+    {
+      id: 9,
+      title: "One-click portfolio generation",
+      category: "feature",
+      upvotes: 62,
+      status: "in_progress",
+      description:
+        "Add ability to create professional looking portfolio from profile.",
+      comments: [
+        {
+          id: 13,
+          content:
+            "I haven't built a portfolio site yet, so this would be really helpful. Might it also be possible to choose layout and colour themes?!",
+          userId: "7",
+        },
+      ],
+    },
+    {
+      id: 10,
+      title: "Bookmark challenges",
+      category: "feature",
+      upvotes: 31,
+      status: "in_progress",
+      description: "Be able to bookmark challenges to take later on.",
+      comments: [
+        {
+          id: 14,
+          content:
+            "This would be great! At the moment, I'm just starting challenges in order to save them. But this means the My Challenges section is overflowing with projects and is hard to manage. Being able to bookmark challenges would be really helpful.",
+          userId: "2",
+        },
+      ],
+    },
+    {
+      id: 11,
+      title: "Animated solution screenshots",
+      category: "bug",
+      upvotes: 9,
+      status: "in_progress",
+      description:
+        "Screenshots of solutions with animations don’t display correctly.",
+    },
+    {
+      id: 12,
+      title: "Add micro-interactions",
+      category: "enhancement",
+      upvotes: 71,
+      status: "live",
+      description: "Small animations at specific points can add delight.",
+      comments: [
+        {
+          id: 15,
+          content:
+            "I'd love to see this! It always makes me so happy to see little details like these on websites.",
+          userId: "11",
+        },
+      ],
+    },
   ];
 }
 
@@ -132,6 +301,20 @@ function getReplies() {
       repliedToId: "6",
       replyFromId: "7",
       commentId: 4,
+    },
+    {
+      commentId: 8,
+      content:
+        "Bumping this. It would be good to have a tab with a feed of people I follow so it's easy to see what challenges they’ve done lately. I learn a lot by reading good developers' code.",
+      repliedToId: "9",
+      replyFromId: "1",
+    },
+    {
+      content:
+        "Me too! I'd also love to see celebrations at specific points as well. It would help people take a moment to celebrate their achievements!",
+      repliedToId: "11",
+      replyFromId: "2",
+      commentId: 15,
     },
   ];
 }
@@ -218,22 +401,3 @@ function getUsers() {
     },
   ];
 }
-
-// replies: {
-//   createMany: {
-//     data: [
-//       {
-//         content:
-//           "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme' followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
-//         repliedFromId: "6",
-//         repliedToId: "5",
-//       },
-//       {
-//         content:
-//           "Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness. I'd prefer not to use one of such extensions, however, for security and privacy reasons.",
-//         repliedToId: "6",
-//         replyFromId: "7",
-//       },
-//     ],
-//   },
-// },
