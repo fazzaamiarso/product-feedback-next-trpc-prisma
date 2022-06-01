f6ac0ed3aa182202c13c24b75a11802bff5dd441
fazzaamiarso <fazzaamiarso@gmail.com>
2022-06-01 20:23:34 +0700

    Add new feedback route

---
+++ src/pages/feedback/new.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const NewFeedback = () => {
+  return (
+    <main>
+      <Link href='/'>
+        <a>Go back</a>
+      </Link>
+      <h1>New Feedbak</h1>
+    </main>
+  );
+};
+
+export default NewFeedback;
