769033bc11f2f0670077348dbe88242d242829fd
fazzaamiarso <fazzaamiarso@gmail.com>
2022-08-22 23:48:27 +0700

    Fix base url?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,15 +45,24 @@ const Auth = ({ children }: { children: ReactNode }) => {
   return null;
 };
 
+
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
 export default withTRPC<AppRouter>({
-  config({ ctx }) {
+  config() {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       transformer: superjson,
